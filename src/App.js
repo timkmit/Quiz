011050391c2194221d3a2e542b63ac833312af92
Timur <timengalytchev@mail.ru>
2023-05-12 23:08:09 +0300

    /

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function Result({correct}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <h2>Вы отгадали {correct} / {questions.length}</h2>
       <a href='/'>
         <button>Попробовать снова</button>
       </a>
